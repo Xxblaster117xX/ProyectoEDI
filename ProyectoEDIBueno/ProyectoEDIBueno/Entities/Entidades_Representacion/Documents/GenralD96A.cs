109f95affaca02b1c2b4a4cd54451f1780bb5980
Xxblaster117xX <141065533+Xxblaster117xX@users.noreply.github.com>
2025-04-16 05:12:08 -0500

    Carpeta Documents creada

---
+++ ProyectoEDIBueno/ProyectoEDIBueno/Entities/Entidades_Representacion/Documents/GenralD96A.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entidades_Representacion.Documents
+{
+    public class GenralD96A
+    {
+    }
+}
---
+++ ProyectoEDIBueno/ProyectoEDIBueno/Entities/Entidades_Representacion/Segments/GroupSegments/GenralD96A/GenralD96ASg5.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entidades_Representacion.Segments.GroupSegments.GenralD96A
+{
+    public class GenralD96ASg5
+    {
+    }
+}
