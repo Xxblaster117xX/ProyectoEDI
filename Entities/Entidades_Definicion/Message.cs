4d9cb277113aeaf95c67ce384083f479dda1a542
Xxblaster117xX <gerrero1944@gmail.com>
2025-04-29 12:38:20 +0200

    Confirmación para creación y modificación de un mensaje hecho, sin embargo falta cargar los css. Id añadido en message

--- Entities/Entidades_Definicion/Message.cs
+++ Entities/Entidades_Definicion/Message.cs
@@ -11,7 +11,7 @@ namespace Entities.Entidades_Definicion
     public class Message
     {
 
-        public int id { get; set; }
+        public int MessageId { get; set; }
 
         [Required]
         public  MessageTypeEnum MessageType;
